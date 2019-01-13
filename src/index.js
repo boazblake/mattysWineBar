1a761e9e14fa1f418ab2e203091ef4652cbf25d4
bblake <bblake@goempyrean.com>
2019-01-13 06:37:56 -0600

    removed jquery

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // index.jsx
-import $ from 'jquery'
 import m from 'mithril'
 const root = document.getElementById('app')
 
@@ -8,29 +7,6 @@ import 'bulma/css/bulma.css'
 import './normalize.css'
 import './index.css'
 import './styles.scss'
-
-//SMOOTH SCROLLING EFFECT
-$(function() {
-	$('a[href*="#"]:not([href="#"])').click(function() {
-		if (
-			location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-			location.hostname == this.hostname
-		) {
-			var target = $(this.hash)
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
-			if (target.length) {
-				$('html, body').animate(
-					{
-						scrollTop: target.offset().top,
-					},
-					1500,
-					'swing'
-				)
-				return false
-			}
-		}
-	})
-})
 
 import App from './App.js'
 
